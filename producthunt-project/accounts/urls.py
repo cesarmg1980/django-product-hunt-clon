9102300e7ea4cc09f1599584f9fbee8e8e572ec0
cesarmg1980 <cesar.m.giulietti@outlook.com>
2020-09-13 17:34:26 -0300

    added login, logout, signup page, and signin logic

--- producthunt-project/accounts/urls.py
+++ producthunt-project/accounts/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from products import views
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home' ),
-    path('accounts/', include('accounts.urls')),
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
 ]
